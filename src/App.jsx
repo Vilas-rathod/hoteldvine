0878fd0067793e23750d29d637014204f0f897b0
Vilas-Rathod <vilasrj958@gmail.com>
2025-02-05 21:30:59 +0530

    add initial files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>Wellcome to Hotel Dvine</h1>
+     <h2>Vilas Rathod</h2>
+    </>
+  )
+}
+
+export default App
