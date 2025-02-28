fa52d39a6f210ff65f31754fc577e37b10c74326
Vilas-Rathod <vilasrj958@gmail.com>
2025-02-28 23:28:05 +0530

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-
-
-// src/App.js
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/common/Navbar';
@@ -9,7 +6,7 @@ import LoginPage from './components/auth/LoginPage';
 import RegisterPage from './components/auth/RegisterPage';
 import HomePage from './components/home/HomePage';
 import AllRoomsPage from './components/booking_rooms/AllRoomsPage';
-import RoomDetailsBookingPage from './components/booking_rooms/RoomDetailsPage';
+import RoomDetailsPage from './components/booking_rooms/RoomDetailsPage';
 import FindBookingPage from './components/booking_rooms/FindBookingPage';
 import AdminPage from './components/admin/AdminPage';
 import ManageRoomPage from './components/admin/ManageRoomPage';
@@ -29,45 +26,37 @@ function App() {
         <div className="content">
           <Routes>
             {/* Public Routes */}
-            <Route exact path="/home" element={<HomePage />} />
-            <Route exact path="/login" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/rooms" element={<AllRoomsPage />} />
             <Route path="/find-booking" element={<FindBookingPage />} />
 
             {/* Protected Routes */}
-            <Route path="/room-details-book/:roomId"
-              element={<ProtectedRoute element={<RoomDetailsBookingPage />} />}
+            <Route
+              path="/room-details-book/:roomId"
+              element={<ProtectedRoute element={<RoomDetailsPage />} />}
             />
-            <Route path="/profile"
+            <Route
+              path="/profile"
               element={<ProtectedRoute element={<ProfilePage />} />}
             />
-            <Route path="/edit-profile"
+            <Route
+              path="/edit-profile"
               element={<ProtectedRoute element={<EditProfilePage />} />}
             />
 
             {/* Admin Routes */}
-            <Route path="/admin"
-              element={<AdminRoute element={<AdminPage />} />}
-            />
-            <Route path="/admin/manage-rooms"
-              element={<AdminRoute element={<ManageRoomPage />} />}
-            />
-            <Route path="/admin/edit-room/:roomId"
-              element={<AdminRoute element={<EditRoomPage />} />}
-            />
-            <Route path="/admin/add-room"
-              element={<AdminRoute element={<AddRoomPage />} />}
-            />
-            <Route path="/admin/manage-bookings"
-              element={<AdminRoute element={<ManageBookingsPage />} />}
-            />
-            <Route path="/admin/edit-booking/:bookingCode"
-              element={<AdminRoute element={<EditBookingPage />} />}
-            />
+            <Route path="/admin" element={<AdminRoute element={<AdminPage />} />} />
+            <Route path="/admin/profile" element={<AdminRoute element={<ProfilePage />} />} />
+            <Route path="/admin/manage-rooms" element={<AdminRoute element={<ManageRoomPage />} />} />
+            <Route path="/admin/edit-room/:roomId" element={<AdminRoute element={<EditRoomPage />} />} />
+            <Route path="/admin/add-room" element={<AdminRoute element={<AddRoomPage />} />} />
+            <Route path="/admin/manage-bookings" element={<AdminRoute element={<ManageBookingsPage />} />} />
+            <Route path="/admin/edit-booking/:bookingCode" element={<AdminRoute element={<EditBookingPage />} />} />
 
             {/* Fallback Route */}
-            <Route path="*" element={<Navigate to="/login" />} />
+            <Route path="*" element={<Navigate to="/home" />} />
           </Routes>
         </div>
         <FooterComponent />
